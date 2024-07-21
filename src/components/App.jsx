efbd3ccc407591c46e831501250a3dc6bd97964c
Ruel Paya <ruel.paya@gmail.com>
2024-07-21 20:47:44 +0800

    Update code
    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,27 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '10
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+        backgroundColor: 'rgb(248, 246, 247)',
+        minHeight: '100vh',
+        padding: '1rem',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2> Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
