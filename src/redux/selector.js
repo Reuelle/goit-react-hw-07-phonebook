54ffcf81cc001f7e45d7eb44300c7201a4c907d9
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-28 16:35:47 +0800

    Update selector.js

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,3 +1,14 @@
+// redux/selector.js
 
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+export const selectFilter = state => state.filter;
+export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectVisibleContacts = state => {
+  const filter = selectFilter(state);
+  const contacts = selectContacts(state);
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+};
