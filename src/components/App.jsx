ad8484d68aa7dce7349c1edb25bf9d4d3c362c5e
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-28 12:20:26 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
