dbe454967dfc3db21ba5331466460eff5735d78d
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-28 13:14:04 +0800

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -11,6 +11,7 @@ export const ContactList = () => {
   if (!contacts) {
     return null;
   }
+
   const visibleContacts = contacts.value.filter(contact =>
     contact.name.toLowerCase().includes(input.toLowerCase())
   );
@@ -18,17 +19,12 @@ export const ContactList = () => {
   return (
     <div className={css.wraperContactList}>
       <ul className={css.contactList}>
-        {visibleContacts.map((contact, id) => (
-          <li key={id} className={css.contactListItem}>
+        {visibleContacts.map((contact) => (
+          <li key={contact.id} className={css.contactListItem}>
             <Contact contact={contact} />
           </li>
         ))}
       </ul>
     </div>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.object,
-  input: PropTypes.string,
 };
