060f652800efaa612c02839b4c2b7cdca2a3d408
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-28 10:16:21 +0800

    Update action.js

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,6 +1,4 @@
 import { nanoid } from 'nanoid';
-
-
 export const ADD_CONTACT = 'contacts/addContact';
 export const DELETE_CONTACT = 'contacts/deleteContact';
 export const SET_FILTER = 'filter/setFilter';
