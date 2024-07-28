22e8df1682541dacfd238a031fa27bacd11ba34d
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-28 13:25:13 +0800

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,12 @@
 import { useDispatch } from 'react-redux';
-import { delContact } from 'redux/slice/contact';
+import { deleteContact } from 'redux/slice/contact'; // Ensure this matches the export from your slice file
 import PropTypes from 'prop-types';
 import css from '../ContactList/ContactList.module.css';
 
 export const Contact = ({ contact }) => {
   const dispatch = useDispatch();
 
-  const handleDelete = () => dispatch(delContact(contact.id));
+  const handleDelete = () => dispatch(deleteContact(contact.id));
 
   return (
     <div>
@@ -25,5 +25,9 @@ export const Contact = ({ contact }) => {
 };
 
 Contact.propTypes = {
-  contact: PropTypes.object,
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
 };
