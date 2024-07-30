78dbff8048f61f4d5fae59ae7aa631f37695c45b
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-30 08:22:47 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 // redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './slice/filter';
-import { contactsReducer } from './slice/contacts'; // Assuming you have contacts reducer
+import { contactsReducer } from './slice/contact'; // Assuming you have contacts reducer
 
 export const store = configureStore({
   reducer: {
