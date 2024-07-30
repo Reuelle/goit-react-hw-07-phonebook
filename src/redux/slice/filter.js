a99147d1652d751cc36cb6e3d77638d12b2185d5
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-30 08:17:23 +0800

    Update filter.js

--- src/redux/slice/filter.js
+++ src/redux/slice/filter.js
@@ -5,9 +5,7 @@ const filterSlice = createSlice({
   name: 'filter',
   initialState: '',
   reducers: {
-    setFilter(state, action) {
-      return action.payload;
-    },
+    setFilter: (state, action) => action.payload,
   },
 });
 
