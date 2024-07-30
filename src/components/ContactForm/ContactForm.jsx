3f10027764a15bbd715269278a745d6907582526
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-30 14:09:54 +0800

    Update ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import css from './ContactForm.module.css';
+import { addContact } from 'redux/slice/contact';
+import { getContacts } from 'redux/selector';
+
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+    form.reset();
+    if (contacts.value.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
+      return false;
+    }
+    dispatch(addContact(name, number));
+    return true;
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+      <label className={css.formLabel}>Name </label>
+      <input
+        className={css.formName}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        placeholder="Enter name"
+      />
+      <label className={css.formLabel}>Number </label>
+      <input
+        className={css.formNumber}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        placeholder="Enter phone number"
+      />
+      <button className={css.formBtn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  contacts: PropTypes.object,
