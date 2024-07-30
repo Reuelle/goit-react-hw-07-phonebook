737aefdb03a888cf2e45a16b2047408867bc995e
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-30 14:22:03 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/slice/contact';
 import { selectContacts } from 'redux/selector';
@@ -13,6 +15,7 @@ export const ContactForm = () => {
     const name = form.elements.name.value;
     const number = form.elements.number.value;
     form.reset();
+    
     if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
       return false;
@@ -29,7 +32,7 @@ export const ContactForm = () => {
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        title="Name may contain only letters, apostrophe, dash and spaces."
         required
         placeholder="Enter name"
       />
@@ -39,7 +42,7 @@ export const ContactForm = () => {
         type="tel"
         name="number"
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +."
         required
         placeholder="Enter phone number"
       />
@@ -48,4 +51,8 @@ export const ContactForm = () => {
       </button>
     </form>
   );
+};
+
+ContactForm.propTypes = {
+  contacts: PropTypes.array, // Ensure this matches the expected type
 };
