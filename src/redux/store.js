35deeefb2a1839fa11973564f51d5708831fbd2c
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-08 16:04:56 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,29 @@
-
 import { configureStore } from '@reduxjs/toolkit';
-import { filterReducer } from './slice/filter';
-import { contactsReducer } from './slice/contact';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 
+import { filterReducer } from './slice/filter';
+import { contactsReducer } from './slice/contact';
+
 const persistConfig = {
-  key: 'root',
+  key: 'root', // This is the key for the persisted state
   storage,
 };
 
+// Combine reducers into a rootReducer
 const rootReducer = combineReducers({
   filter: filterReducer,
   contacts: contactsReducer,
 });
 
+// Wrap rootReducer with persistReducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Create store with the persisted reducer
 export const store = configureStore({
   reducer: persistedReducer,
 });
 
+// Create persistor to manage the persisted state
 export const persistor = persistStore(store);
